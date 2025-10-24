84898c926bc8ab6519cb038496e26b8fe8dc86bd
Mavlon4658 <mavlonbe4658>
2025-10-24 08:47:16 +0500

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+const bodyHidden = () => {
+    document.querySelector('body').style.overflow = 'hidden';
+}
+
+const bodyVisible = () => {
+    document.querySelector('body').style.overflow = 'visible';
+}
+
+const phoneInp = document.querySelectorAll('input[type="tel"]');
+
+if (phoneInp.length) {
+    phoneInp.forEach(el => {
+        IMask(el, {
+            mask: '+{7}(000) 000-00-00',
+        })
+    });
+}
